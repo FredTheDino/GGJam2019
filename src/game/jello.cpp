9e18bc7c84120fe698aae92d526a7dd42a4b28a5
Gabriel Tofvesson <gabto095@student.liu.se>
2019-01-26 19:53:51 +0100

    Sounds! :D

--- src/game/jello.cpp
+++ src/game/jello.cpp
@@ -9,6 +9,8 @@ bool jello_on_collision(Body *self, Body *other, Overlap overlap)
 
 	other->velocity.y = BOUNCE_SPEED;
 	
+    play_sound_perturbed(audio_splat, 1, 1, 0.5, 1);
+
 	return false;
 }
 Jello *create_jello(Shot *shot)
