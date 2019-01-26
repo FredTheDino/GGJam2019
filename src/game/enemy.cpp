1cab9011cc858649b441cf8453a2429a6dbe1644
AxelMatstoms <axel.matstoms@gmail.com>
2019-01-26 17:40:35 +0100

    cod and epicest mackfil

---
+++ src/game/enemy.cpp
@@ -0,0 +1,31 @@
+#define ENEMY_ACCELERATION 10.0f
+#define ENEMY_DEACCELERATION 40.0f
+#define ENEMY_MAX_SPEED 5
+#define ENEMY_START_HP 3
+
+struct Enemy {
+    BodyId body_id;
+    u32 hp;
+};
+
+bool enemy_callback(Body *self, Body *other, Overlap overlap)
+{
+    Enemy *enemy = (Enemy *) self->self;
+}
+
+Enemy *create_enemy()
+{
+    Enemy *enemy = push_struct(Enemy);
+    enemy->body_id = create_body(0xff, 1, 0);
+    enemy->body_id->self = enemy;
+    enemy->body_id->velocity.x = 
+    enemy->hp = ENEMY_START_HP;
+
+    return enemy;
+}
+
+void destroy_enemy(Enemy *enemy)
+{
+    destroy_body(enemy->body_id);
+    pop_memory(enemy);
+}
