47bcc4c56ae642f9a138a0080e2679a4fe083ed2
AxelMatstoms <axel.matstoms@gmail.com>
2019-01-27 15:44:50 +0100

    can kill steak

--- src/game/enemy.cpp
+++ src/game/enemy.cpp
@@ -7,7 +7,7 @@
 
 struct Enemy {
     BodyID body_id;
-    u32 hp;
+    s32 hp;
     s32 facing;
 	bool crying;
 	f32 animation_timer;
@@ -20,20 +20,17 @@ bool enemy_callback(Body *self, Body *other, Overlap overlap)
     Enemy *enemy = (Enemy *) self->self;
     if (other->type == SHOT_TYPE) {
 	Shot *shot = (Shot *) other->self;
-    if (shot->shot_kind == ONION) {
-        enemy->crying = true;
-    }
-	destroy_shot(shot);
-	enemy->hp--;
-	if (enemy->hp <= 0) {
-            destroy_enemy(enemy);
-	}
-	return 1;
+        if (shot->shot_kind == ONION) {
+            enemy->crying = true;
+        }
+	    //destroy_shot(shot);
+	    enemy->hp--;
+	    return 1;
     } else if (other->inverse_mass == 0) {
-	s32 dir = overlap.normal.x;
-	if (dir)
-		enemy->facing = -dir;
-	return 0;
+	    s32 dir = overlap.normal.x;
+	    if (dir)
+		    enemy->facing = -dir;
+	    return 0;
     }
     return 0;
 }
@@ -78,13 +75,14 @@ void destroy_enemy(Enemy *enemy)
     pop_memory(enemy);
 }
 
-void enemy_update(Enemy *enemy, f32 delta)
+bool enemy_update(Enemy *enemy, f32 delta)
 {
     BodyID body_id = enemy->body_id;
     Vec2 *vel = &body_id->velocity;
     vel->y -= GRAVITY * delta;
     vel->x = ENEMY_MAX_SPEED * enemy->facing;
     enemy->animation_timer += delta;
+    return enemy->hp <= 0;
 }
 
 void enemies_draw (List<Enemy*> *enemies) 
@@ -101,7 +99,11 @@ void update_enemies (List<Enemy*> *enemies, f32 delta)
 	for (s32 i = enemies->length-1; i >= 0; i--) 
 	{
 		Enemy *enemy = (*enemies)[i]; 
-		enemy_update(enemy, delta);
+		bool is_dead = enemy_update(enemy, delta);
+        if (is_dead) {
+            destroy_enemy(enemy);
+            enemies->remove(i);
+        }
 	}
 }
 
